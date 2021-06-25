416d1b08ef02414e2da24303c9e17588f7080b8b
wjxor <shko940575@gmail.com>
2021-06-26 02:44:36 +0900

    URL 기본

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 var http = require("http");
 var fs = require("fs");
+
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == "/") {
-    url = "/index.html";
+  var _url = request.url;
+  var myURL = new URL('http://localhost:3000' + _url);
+  var queryData = myURL.searchParams.get('id');
+
+  console.log(queryData.id);
+
+  if (_url == "/") {
+    _url = "/index.html";
   }
 
-  if (request.url == "/favicon.ico") {
+  if (_url == "/favicon.ico") {
     response.writeHead(404);
     response.end();
     return;
   }
 
   response.writeHead(200);
-  console.log(__dirname + url);
-  response.end(fs.readFileSync(__dirname + url));
+  response.end(queryData.id);
+  // response.end(fs.readFileSync(__dirname + _url));
 });
 
 // 포트
